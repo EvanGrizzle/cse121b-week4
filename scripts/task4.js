95da3a9c8c694dab0bbb38b1b525ba5cc1c7f1c3
EvanGrizzle <evangrizzle15@gmail.com>
2022-10-08 18:53:59 -0400

    completed part 1 of task4.js

--- scripts/task4.js
+++ scripts/task4.js
@@ -4,6 +4,23 @@
 
 // Step 1: Declare a new variable to hold information about yourself
 
+const mySelf = {
+    name: "Evan Grizzle",
+    photo: "images/face.jpg",
+    favoriteFoods: ["Hot Wings", "Pizza", "Sub Sandwiches"],
+    hobbies: ["Reading", "Movies", "Board Games"],
+    placesLived: [
+        {
+            place: "Georgia",
+            length: "26.5 Years"
+        },
+        {
+            place: "Idaho",
+            length: "6 Months"
+        }
+    ]
+};
+
 // Step 2: Inside of the object, add a property named name with a value of your name as a string
 
 // Step 3: Add another property named photo with a value of the image path and name (used in Task 2) as a string
